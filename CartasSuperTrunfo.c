02424c4500c74cee4606cdd3c3473394f43a3722
amanda4615 <francisco.zanela4@gmail.com>
2025-02-10 22:11:30 +0000

    carta Rio o1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
   
 int main(){
-    printf("paises!");
-    printf("cadastro das cartas!");
+    char nome[20];
+    int codigo;
+    int populacao;
+    int area;
+    int pib;
+    int turistico;
     
-  
+    printf("digite o nome da cidade\n");
+    scanf(" %s", &nome);
 
+    printf("digite o codigo da cidade\n");
+    scanf(" %d", &codigo);
+
+    printf("digite a população\n");
+    scanf(" %f",  &populacao);
     
-    
+    printf("digite a area\n");
+    scanf(" %f", &area);
 
+    printf("digite pib\n");
+    scanf(" %f", &pib);
 
-
+    printf("digite o numero de pontos turisticos\n");
+    scanf(" %d", &turistico);
 
 return 0;
 
