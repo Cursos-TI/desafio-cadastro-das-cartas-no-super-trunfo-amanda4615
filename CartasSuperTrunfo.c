05668bdb2577e03dcbc404c0ac92482e0b56f35c
amanda4615 <francisco.zanela4@gmail.com>
2025-02-12 14:56:05 +0000

    cadastro DO ESTADO A

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,34 +1,6 @@
 #include <stdio.h>
   
 int main(){
-    char nome[20];
-    int codigo;
-    int populacao;
-    int area;
-    int pib;
-    int turistico;
     
-    printf("digite o nome da cidade\n");
-    scanf(" %s", &nome);
-
-    printf("digite o codigo da cidade\n");
-    scanf(" %d", &codigo);
-
-    printf("digite a população\n");
-    scanf(" %f",  &populacao);
+    printf("carta A01!");
     
-    printf("digite a area\n");
-    scanf(" %f", &area);
-
-    printf("digite pib\n");
-    scanf(" %f", &pib);
-
-    printf("digite o numero de pontos turisticos\n");
-    scanf(" %d", &turistico);
-
-return 0;
-
-}
-
- 
-
