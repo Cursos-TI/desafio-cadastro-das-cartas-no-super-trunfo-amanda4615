e38a3a6ddfdd8aaf95a956b4d46c0c7d63e6f15c
amanda4615 <francisco.zanela4@gmail.com>
2025-02-12 20:07:47 +0000

    cadastro das variaveis das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,3 +4,19 @@ int main(){
     
     printf("carta A01!");
     
+    char Estado;
+    char codigo[20];
+    char cidade[20];
+    int populacao;
+    float area(km²);
+    float PIB;
+    int pontos (turisticos);
+
+    
+
+    return 0;
+
+}
+
+
+    
