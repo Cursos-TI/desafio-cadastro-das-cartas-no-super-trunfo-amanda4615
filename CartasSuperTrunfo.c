7f81b1645e1642e806fe9bc289c5f221f439171f
amanda4615 <francisco.zanela4@gmail.com>
2025-02-13 13:22:06 +0000

    Cadastro da Carta A02

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,8 @@
   
 int main(){
     
-    printf("carta A01\n");
+    printf("carta A02\n");
+
     
     
     char estado;
@@ -20,49 +21,81 @@ int main(){
     printf("digite o codigo da carta:");
     scanf(" %s", &codigo_da_carta);
 
-
-    
+   
     printf("digite a cidade:");
     scanf(" %s", &cidade);
 
+   
     printf("digite a populacao:");
     scanf(" %d", &populacao);
 
+   
     printf("digite a area:");
     scanf(" %f", &area_km2);
 
+    
     printf("digite o pib:");
     scanf(" %f", &pib);
 
+    
     printf("digite total ponto turisticos:");
     scanf(" %d", &pontos_turisticos);
 
-    printf("carta A02\N");
-    
-    
-    
-
-
-
-
-
-
-
-
+   
 
     
-
-
-
-
-
-
-
-    
-
-    return 0;
+return 0;
 
 }
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
+
+ 
+
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
 
 
     
