5df418167a6b7b6ef7a276e1d3b3ba66a9d133fe
amanda4615 <francisco.zanela4@gmail.com>
2025-02-15 20:26:41 +0000

    CADASTRO DAS CARTAS  DE A a H

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
   
 int main(){
     
-    printf("carta A04\n");
+    printf("carta A01\n");
 
     char estado;
     char codigo_da_carta[3];
@@ -41,8 +41,1171 @@ int main(){
 
    
 
+
+
+    printf("carta A02\n");
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+
+    printf("carta A03\n");
+
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+
+    printf("carta A04\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+    
+    printf("carta B01\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+   
+
+
+    printf("carta B02\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+   
+    printf("carta B03\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+    
+    printf("carta B04\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+    printf("carta C01\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta C02\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+
+    printf("carta C03\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+    
+    printf("carta C04\N");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+   
+    printf("carta D01\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+    
+    printf("carta D02\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta D03\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+   
+    printf("carta D04\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta E01\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta E02\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta E03\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta E04\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta F01\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta F02\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta F03\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta F04\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta G01\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta G02\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta G03\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta G04\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta H01\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta H02\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta H03\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+    printf("carta H04\n");
+
+   
+    printf("Digite o Estado:");
+    scanf(" %c", &estado);
+    
+    printf("digite o codigo da carta:");
+    scanf(" %s", &codigo_da_carta);
+
+   
+    printf("digite a cidade:");
+    scanf(" %s", &cidade);
+
+   
+    printf("digite a populacao:");
+    scanf(" %d", &populacao);
+
+   
+    printf("digite a area:");
+    scanf(" %f", &area_km2);
+
+    
+    printf("digite o pib:");
+    scanf(" %f", &pib);
+
+    
+    printf("digite total ponto turisticos:");
+    scanf(" %d", &pontos_turisticos);
+
+
+
 return 0;
 }
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
 
 
     
