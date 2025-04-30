44b164db67df9c93b637a84287379a2de382bbdf
amanda4615 <francisco.zanela4@gmail.com>
2025-04-30 18:43:45 +0000

    defini variaveis, for p mover a torre, do while para , bispo, while para rainha .

--- CartasSuperTrunfo-Intermediario.c
+++ CartasSuperTrunfo-Intermediario.c
@@ -1,125 +1,42 @@
 #include <stdio.h>
-#include <stdbool.h>
-  
+
+
 int main(){
-    
-    char estado_carta1[3];
-    char codigo_da_carta_carta1[3];
-    char cidade_carta1[50];
-    char estado_carta2[3];
-    char codigo_da_carta_carta2[3];
-    char cidade_carta2[50];
-    
-    int pontos_turisticos_carta1;
-    int pontos_turisticos_carta2;
-    float populacao_carta1;
-    float populacao_carta2;
-    float area_km2_carta1;
-    float area_km2_carta2;
-    float pib_carta1;
-    float pib_carta2;
-    float pib_per_capta_carta1;
-    float pib_per_capta_carta2;
-    float densidade_populacional_carta1;
-    float densidade_populacional_carta2;
-    float super_poder_carta1;
-    float super_poder_carta2;
-
-    printf("\n\n");   
-    printf("carta 1\n");
-
-    printf("Digite o estado: ");
-    scanf(" %s", estado_carta1);
-
-    printf("digite o codigo da carta: ");
-    scanf(" %s", codigo_da_carta_carta1);
-
-    printf("digite a cidade: ");
-    scanf(" %s", cidade_carta1);
+   // definindo variaveis
+   int T;
+   int B = 1;
+   int R = 1;
    
-    printf("digite a população: ");
-    scanf(" %f", &populacao_carta1);
-   
-    printf("digite a area: ");
-    scanf(" %f", &area_km2_carta1);
-
-    printf("digite o pib: ");
-    scanf(" %f", &pib_carta1);
-
-    printf("digite total ponto turisticos: ");
-    scanf(" %d", &pontos_turisticos_carta1);
-
-    printf("densidade populacional: ");
-    densidade_populacional_carta1 = populacao_carta1 / area_km2_carta1;
-    printf("%.2f\n", densidade_populacional_carta1);
-
-    printf("pib per capta: ");
-    pib_per_capta_carta1 = pib_carta1 / populacao_carta1;
-    printf("%.2f\n", pib_per_capta_carta1);
-
-
-    printf("super poder: ");
-    super_poder_carta1 = pontos_turisticos_carta1 + area_km2_carta1 + pib_carta1 + pib_per_capta_carta1 + populacao_carta1 + (1 / densidade_populacional_carta1);
-    printf("%.2f\n", super_poder_carta1);
-
+   // mover a torre cinco vezes para a direira.
+   for (int i = 0;i <+ 5; i++){
+      printf("Direita\n"); // imprime a direção do movimento.
     
-
-    //
-    // CARTA 2
-    //
-  
-    printf("\n\n");  
-    printf("carta 2\n");
-   
-    printf("Digite o Estado:");
-    scanf(" %s", estado_carta2);
+      //mover o bispo para cima e para direita.
+   }
+   do
+   {
+     printf("Cima, Direira\n");// imprime a direção do movimento.
+     B++;//incrementa o contador.
     
-    printf("digite o codigo da carta:");
-    scanf(" %s", codigo_da_carta_carta2);
+   } while (B <= 5);//incrementa o contador for menor ou igual a 5.
 
-    printf("digite a cidade:");
-    scanf(" %s", cidade_carta2);
+   // mover a Rainha oito vezes para a esquerda.
+   while (R <= 8) {
+     printf("Esquerda\n");// imprime a direção do movimento.
+      R++;//incremnta o contador.
+   }
+    /* code */
+   
+   
+       return 0;
 
-    printf("digite a populacao:");
-    scanf(" %f", &populacao_carta2);
-
-    printf("digite a area:");
-    scanf(" %f", &area_km2_carta2);
-
-    printf("digite o pib:");
-    scanf(" %f", &pib_carta2);
-
-    printf("digite total ponto turisticos:");
-    scanf(" %d", &pontos_turisticos_carta2);
-
-    printf("densidade populacional: ");
-    densidade_populacional_carta2 = populacao_carta2/ area_km2_carta2;
-    printf(".2%f\n", densidade_populacional_carta2);
-
-    printf("pib per capta: ");
-    pib_per_capta_carta2 = pib_carta2 / populacao_carta2;
-    printf("%.2f\n", pib_per_capta_carta2);
-
-    printf("super_poderes_carta2: ");
-    pib_per_capta_carta2 = pontos_turisticos_carta2 + populacao_carta2 + area_km2_carta2 + pib_carta2 + pib_per_capta_carta2 + densidade_populacional_carta2;
-    printf(".2%f\n", pib_per_capta_carta2);
+      }
 
 
-    //
-    // comparação if e esle entre area carta1 e area carta.
-    //
-    if ( area_km2_carta1 > area_km2_carta2) {
-      printf("Carta1 venceu!\n");
-      printf("cidade: %s, valor da carta = %.2f\n", cidade_carta1, area_km2_carta1);
       
-     } else if ( area_km2_carta1 < area_km2_carta2) {
-      printf("Carta2 venceu!\n");
-      printf("cidade: %s, valor da carta = %.2f\n", cidade_carta2, area_km2_carta2);
 
-    } else {
-        printf("houve um empate!\n");
+    
+         
 
-    }
 
-    return 0;
-}
+
